665018d4be6682100317f10a9e098c6df3433545
GiorgioCassata <38116994+GiorgioCassata@users.noreply.github.com>
2019-03-29 17:00:08 -0600

    Low-key works, just prints out results to console

--- main.cpp
+++ main.cpp
@@ -2,7 +2,7 @@
 #include <iostream>
 #include <vector>
 #include <string>
-
+#include <cmath>
 
 using namespace std;
 
@@ -35,31 +35,38 @@ int main() {
     vector<vector<int>> list;
     vector<int> x;
     vector<int> s;
+
     int numDays;
     int temp;
+    bool trigger;
     ifstream file;
 
     // read in and store into vectors, the x and s series  of numbers
     file.open("input.txt");
     file >> numDays;
     vector<vector<int>> processes (numDays);
+    vector<int> sums (pow(numDays-1,2),0);
     for (int i = 0; i < numDays; ++i) {
         file >> temp;
         x.push_back(temp);
+        //cout << temp << ", ";
     }
+    //cout << endl;
     for (int i = 0; i < numDays; ++i) {
         file >> temp;
         s.push_back(temp);
+        //cout << temp << ", ";
     }
+    //cout << endl;
     file.close();
 
     // calculate all possible processes needed
     for (int i = 0; i < numDays; ++i) {
         for (int j = 0; j < numDays; ++j) {
-            if (s.at(i) < x.at(j)) {
-                processes.at(i).push_back(s.at(i));
+            if (s.at(j) > x.at(i)) {
+                processes.at(i).push_back(x.at(i));
             } else {
-                processes.at(i).push_back(x.at(j));
+                processes.at(i).push_back(s.at(j));
             }
             if (j >= i) {
                 break;
@@ -79,14 +86,33 @@ int main() {
 
 
     list = Function(numDays-1, list);
+    list.insert(list.begin(), {0});
+    for (int k = 0; k < sums.size()-1; ++k) {
+        temp = 0;
+        for (int i = 0; i < numDays; ++i) {
+            trigger = false;
+            for (int a = 0; a < list.at(k).size(); ++a) {
+                if (list.at(k).at(a)-1 == i) {
+                    temp = 0;
+                    trigger = true;
+                    break;
+                }
+            }
+            if (trigger) {
+                continue;
+            }
+            sums.at(k) += processes.at(i).at(temp);
+            temp++;
+        }
+    }
 
 
-    // print list to console
+    // print list to console w/ sums
     for (int i = 0; i < list.size(); ++i) {
         for (int j = 0; j < list.at(i).size(); ++j) {
             cout << list.at(i).at(j) << ", ";
         }
-        cout << endl;
+        cout << "   , " << sums.at(i) << endl;
     }
     cout << endl;
 
