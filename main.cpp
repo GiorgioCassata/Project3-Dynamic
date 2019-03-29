2edecb085efaaa3c9938f97e85b980565b59b17e
GiorgioCassata <38116994+GiorgioCassata@users.noreply.github.com>
2019-03-28 22:54:22 -0600

    First Pseudo to code

---
+++ main.cpp
@@ -0,0 +1,45 @@
+#include <fstream>
+#include <iostream>
+#include <vector>
+#include <string>
+
+
+using namespace std;
+
+// A recursive function that builds all combinations of
+// days with no repeats and without considering order
+vector<vector<int>> Function(int n, vector<vector<int>> list) {
+    vector<vector<int>> list2;
+  // stop condition for recursion
+  if (n == 0) {
+      return list;
+  }
+
+  // add current day
+  list2.push_back({n});
+
+  list2.insert(list2.end(), list.begin(), list.end());
+  // adding current day to all previous combinations
+    for (int i = 1; i < list2.size(); ++i) {
+        list2.at(i).push_back(n);
+    }
+
+  list.insert(list.end(), list2.begin(), list2.end());
+  // recursive call to calculate combinations for next smaller size
+  return Function(n-1, list);
+}
+
+//4,3,43,2,42,32,432,1,41,31,431,21,421,321,4321
+
+int main() {
+    vector<vector<int>> list;
+    list = Function(4, list);
+    for (int i = 0; i < list.size(); ++i) {
+        for (int j = 0; j < list.at(i).size(); ++j) {
+            cout << list.at(i).at(j) << ", ";
+        }
+        cout << endl;
+    }
+    cout << endl;
+    return 0;
+}
